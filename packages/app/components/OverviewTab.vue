b6eef326483904cdafd26c86e4da51c7035ab5ad
switzerchees <patrickmichel96@yahoo.de>
2023-04-09 10:56:40 +0200

    Add markdown for summary

--- packages/app/components/OverviewTab.vue
+++ packages/app/components/OverviewTab.vue
@@ -4,12 +4,7 @@
       <OverviewQuestions />
     </TabPanel>
     <TabPanel header="Zusammenfassung">
-      <p>
-        Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae
-        ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit
-        aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Consectetur, adipisci
-        velit, sed quia non numquam eius modi.
-      </p>
+      <Markdown :source="selectedLesson?.summary || selectedCourse?.summary" class="markdown-format" />
     </TabPanel>
     <TabPanel header="Material">
       <p>
@@ -29,3 +24,9 @@
     </TabPanel>
   </TabView>
 </template>
+
+<script setup lang="ts">
+  import { storeToRefs } from 'pinia'
+
+  const { selectedCourse, selectedLesson } = storeToRefs(useSidebarsStore())
+</script>
